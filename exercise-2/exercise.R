742308263a7c77e8feb9674e743d53771e81d574
mkfreeman <michael.k.freeman@gmail.com>
2016-11-27 15:37:38 -0800

    Removed exercise-2 answers

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -32,26 +32,24 @@ for(i in 1:100) {
   # Create test and training data
   # Hint: http://stackoverflow.com/questions/17200114/how-to-split-data-into-training-testing-sets-using-sample-function-in-r-program
   # 1. Create training and testing datasets by sampling 75% of your data from your `homes` dataframe.
-  train.indicies <- sample(seq_len(nrow(homes)), size = sample.size)
-  training.data <- homes[train.indicies,]
-  test.data <- homes[-train.indicies,]
+  
   
   # 2. Pass your **training data** to the `rpart` function to run a simple classification operation
-  basic.fit <- rpart(in_sf ~ ., data = training.data, method="class")
+  
   
   # 3. Pass your results to the `AssessFit` function to assess the fit
-  assessment <- AssessFit(basic.fit, data=test.data)
+  
   
   # 4. Store your assessment in the `basic.fits` vector
-  basic.fits <- c(basic.fits, assessment)
+  
 }
 
 # 5. Make a histogram of your `basic.fits` vector
-hist(basic.fits)
+
 
 # 6. Take the mean of your `basic.fits` vector
-mean(basic.fits)
+
 
 
 # 7. Pass your most recent model to the `rpart.plot` function to graph it
-rpart.plot(basic.fit)
+
