365afc97c774a2c6c4fd91420840516f34f7f501
Ryan Zuzelski <rzuzelski@gmail.com>
2016-11-29 15:09:58 -0800

    did stuff

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,7 +4,7 @@ library(rpart)
 library(rpart.plot)
 
 # Read in data
-setwd('~/Documents/info-201/m15-special-topics/exercise-2')
+setwd('~/INFO201/m15-special-topics/exercise-2')
 homes <- read.csv('data/housing-data.csv')
 
 # Function to compare values
@@ -32,16 +32,19 @@ for(i in 1:100) {
   # Create test and training data
   # Hint: http://stackoverflow.com/questions/17200114/how-to-split-data-into-training-testing-sets-using-sample-function-in-r-program
   # 1. Create training and testing datasets by sampling 75% of your data from your `homes` dataframe.
+  train_ind <- sample(seq_len(nrow(homes)), size = sample.size)
   
+  train <- homes[train_ind, ]
+  test <- homes[-train_ind, ]
   
   # 2. Pass your **training data** to the `rpart` function to run a simple classification operation
-  
+  fit <- rpart(price ~ ., data=train, method="class")
   
   # 3. Pass your results to the `AssessFit` function to assess the fit
-  
+  fit_assess <- AssessFit(fit, outcome="price")
   
   # 4. Store your assessment in the `basic.fits` vector
-  
+  basic.fits <- c(basic.fits, fit_assess)
 }
 
 # 5. Make a histogram of your `basic.fits` vector
